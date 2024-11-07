1f44c5c553352c5dd894067b32aeac648d0b2d94
DoomchitYJ <poj1004@inha.edu>
2024-11-08 01:07:33 +0900

    fix(ProductsRepository): List<List<String, String>> 형태로 재고를 반환하도록 수정

--- src/main/java/store/repository/ProductsRepository.java
+++ src/main/java/store/repository/ProductsRepository.java
@@ -3,17 +3,44 @@ package store.repository;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProductsRepository {
 
+    private static final int PRODUCTS_START_INDEX = 1;
+
+    private static final String DELIMITER = ",";
     private final Path filePath;
 
     public ProductsRepository(final Path filePath) {
         this.filePath = filePath;
     }
 
-    public List<String> readProductsFile() throws IOException {
-        return Files.readAllLines(filePath);
+    public List<List<String>> readProductsFile() throws IOException {
+        List<String> lines = Files.readAllLines(filePath);
+        String[] header = lines.getFirst().split(DELIMITER);
+
+        List<List<String>> products = addProducts(lines, header);
+
+        return products;
+    }
+
+    private List<List<String>> addProducts(List<String> lines, String[] header) {
+        List<List<String>> products = new ArrayList<>();
+
+        for (int i = PRODUCTS_START_INDEX; i < lines.size(); i++) {
+            String[] line = lines.get(i).split(DELIMITER);
+            List<String> productInfo = new ArrayList<>();
+
+            for (int j = 0; j < header.length; j++) {
+                productInfo.add(line[j].trim());
+            }
+            products.add(productInfo);
+        }
+
+        return products;
     }
 }
