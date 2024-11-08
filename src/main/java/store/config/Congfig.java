81322ab1bfe8bfff0d3dd8ede43a012fe83b125f
DoomchitYJ <poj1004@inha.edu>
2024-11-08 13:43:45 +0900

    feat(Config): Products.md와 Promotions.md의 위치 정보를 저장하는 Config 구현

---
+++ src/main/java/store/config/Congfig.java
@@ -0,0 +1,6 @@
+package store.config;
+
+public class Congfig {
+    public static final String PRODUCTS_FILE_PATH = "src/main/resources/products.md";
+    public static final String PROMOTIONS_FILE_PATH = "src/main/resources/promotions.md";
+}
