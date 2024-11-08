588df19c0803de7d1b448899d4e137fa360619f7
DoomchitYJ <poj1004@inha.edu>
2024-11-08 20:23:05 +0900

    feat(StoreException): 예외 처리를 위한 커스텀 예외 구현

---
+++ src/main/java/store/exception/ExceptionMessage.java
@@ -0,0 +1,21 @@
+package store.exception;
+
+public enum ExceptionMessage {
+
+    INVALID_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
+    NON_EXISTENT_PRODUCT("존재하지 않는 상품입니다. 다시 입력해 주세요."),
+    TOO_MANY_ORDER("재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
+    NOT_YES_OR_NO("Y 혹은 N을 입력해주세요.");
+
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    private final String message;
+
+    ExceptionMessage(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_PREFIX + message;
+    }
+}
