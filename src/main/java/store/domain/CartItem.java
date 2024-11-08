753258c966f1f16ae4ae4530b77a138f1f31eee2
DoomchitYJ <poj1004@inha.edu>
2024-11-09 01:21:46 +0900

    feat(CartItem): CartItem에 프로모션으로 증정 받은 물건의 개수를 프로퍼티로 저장

--- src/main/java/store/domain/CartItem.java
+++ src/main/java/store/domain/CartItem.java
@@ -4,10 +4,12 @@ public class CartItem {
 
     private Product product;
     private int quantity;
+    private int free;
 
-    public CartItem(final Product product, final int quantity) {
+    public CartItem(final Product product, final int quantity, final int free) {
         this.product = product;
         this.quantity = quantity;
+        this.free = free;
     }
 
 }
