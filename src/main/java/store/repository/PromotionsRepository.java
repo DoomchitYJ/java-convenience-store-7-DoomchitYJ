24d484cff66ad92522dcf57f95f99cd1e1175cfd
DoomchitYJ <poj1004@inha.edu>
2024-11-08 21:15:07 +0900

    fix(PromotionsRepository): List<List<String, String>> 형태로 재고를 반환하도록 수정

--- src/main/java/store/repository/PromotionsRepository.java
+++ src/main/java/store/repository/PromotionsRepository.java
@@ -1,19 +1,46 @@
 package store.repository;
 
+import static store.constant.Constant.DELIMITER;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PromotionsRepository {
 
+    private static final int PROMOTIONS_START_INDEX = 1;
+
     private final Path filePath;
 
-    public PromotionsRepository(final Path filePath) {
-        this.filePath = filePath;
+    public PromotionsRepository(final String filePath) {
+        this.filePath = Paths.get(filePath);
     }
 
-    public List<String> readPromotionsFile() throws IOException {
-        return Files.readAllLines(filePath);
+    public List<List<String>> readPromotionsFile() throws IOException {
+        List<String> lines = Files.readAllLines(filePath);
+        String[] header = lines.getFirst().split(DELIMITER);
+
+        List<List<String>> promotions = addPromotions(lines, header);
+
+        return promotions;
+    }
+
+    private List<List<String>> addPromotions(List<String> lines, String[] header) {
+        List<List<String>> promotions = new ArrayList<>();
+
+        for (int i = PROMOTIONS_START_INDEX; i < lines.size(); i++) {
+            String[] line = lines.get(i).split(DELIMITER);
+            List<String> promotionInfo = new ArrayList<>();
+
+            for (int j = 0; j < header.length; j++) {
+                promotionInfo.add(header[j].trim());
+            }
+            promotions.add(promotionInfo);
+        }
+
+        return promotions;
     }
 }
