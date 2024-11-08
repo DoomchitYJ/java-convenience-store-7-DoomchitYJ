d08bdd544a0e4a718d29655e1c68e6ad51725d61
DoomchitYJ <poj1004@inha.edu>
2024-11-08 14:29:21 +0900

    feat(Product): 프로퍼티를 반환하는 메서드 구현

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -8,16 +8,28 @@ public class Product {
 
     private static int quantity;
 
-    private static String description;
+    private static String promotion;
 
-    public Product(final String name, final int price, final int quantity, final String description) {
+    public Product(final String name, final int price, final int quantity, final String promotion) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.description = description;
+        this.promotion = promotion;
     }
 
     public void updateQuantity(final int amount) {
         quantity += amount;
+    }
+
+    public static int getName() {
+        return quantity;
+    }
+
+    public static int getPrice() {
+        return price;
+    }
+
+    public static String getPromotion() {
+        return promotion;
     }
 }
