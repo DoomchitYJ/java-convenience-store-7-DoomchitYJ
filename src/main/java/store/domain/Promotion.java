8c6f56ac987963e730b1c1351ac57055be04fb59
DoomchitYJ <poj1004@inha.edu>
2024-11-08 21:40:15 +0900

    feat(Promotion): 프로모션 정보를 담는 Promotion 도메인 구현

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,44 @@
+package store.domain;
+
+import java.util.Date;
+
+public class Promotion {
+
+    private String name;
+
+    private int buy;
+
+    private int get;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    public Promotion(String name, int buy, int get, Date startDate, Date endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+}
