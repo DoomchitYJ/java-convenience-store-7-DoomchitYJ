621b3f9f4696c575b54737021bfd6a5b37c68975
DoomchitYJ <poj1004@inha.edu>
2024-11-08 14:57:58 +0900

    refactor(InventoryService): 프로모션이 null일 경우의 처리 로직의 위치 변경
    - 기존 OutputView에서 동작하던 로직을 재고를 셋업할 때 동작하도록 위치를 변경

--- src/main/java/store/service/InventoryService.java
+++ src/main/java/store/service/InventoryService.java
@@ -5,6 +5,7 @@ import static store.constant.Constant.INDEX_NAME;
 import static store.constant.Constant.INDEX_PRICE;
 import static store.constant.Constant.INDEX_PROMOTION;
 import static store.constant.Constant.INDEX_QUANTITY;
+import static store.constant.Constant.NULL;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,18 +17,32 @@ public class InventoryService {
 
     public static List<Product> setUpInventory() {
         List<Product> inventory = new ArrayList<Product>();
-
         ProductsRepository productsRepository = new ProductsRepository(PRODUCTS_FILE_PATH);
         try {
             List<List<String>> products = productsRepository.readProductsFile();
             for (List<String> product : products) {
-                Product item = new Product(product.get(INDEX_NAME), Integer.parseInt(product.get(INDEX_PRICE)),
-                        Integer.parseInt(product.get(INDEX_QUANTITY)), product.get(INDEX_PROMOTION));
+                Product item = convertIntoProduct(product);
                 inventory.add(item);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return inventory;
+    }
+
+    private static Product convertIntoProduct(List<String> product) {
+        String name = product.get(INDEX_NAME);
+        int price = Integer.parseInt(product.get(INDEX_PRICE));
+        int quantity = Integer.parseInt(product.get(INDEX_QUANTITY));
+        String promotion = getPromotion(product);
+
+        return new Product(name, price, quantity, promotion);
+    }
+
+    private static String getPromotion(List<String> product) {
+        String promotion = product.get(INDEX_PROMOTION);
+        if (promotion.equals(NULL)) { return null; }
+
+        return promotion;
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,11 +1,7 @@
 package store.view;
 
-import static store.constant.Constant.INDEX_NAME;
-import static store.constant.Constant.INDEX_PRICE;
-import static store.constant.Constant.INDEX_PROMOTION;
-import static store.constant.Constant.INDEX_QUANTITY;
-
-import java.util.List;
+import store.domain.Inventory;
+import store.domain.Product;
 
 public class OutputView {
 
@@ -13,24 +9,17 @@ public class OutputView {
 
     private static final String STORE_START_VIEW = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.";
 
-    public static void showProducts(List<List<String>> products) {
+    public static void showProducts(Inventory inventory) {
         System.out.println(STORE_START_VIEW);
 
-        for (List<String> product : products) {
-            String name = product.get(INDEX_NAME);
-            int price = Integer.parseInt(product.get(INDEX_PRICE));
-            int quantity = Integer.parseInt(product.get(INDEX_QUANTITY));
-            String promotion = getPromotion(product);
+        for (Product product : inventory.getProducts()) {
+            String name = product.getName();
+            int price = product.getPrice();
+            int quantity = product.getQuantity();
+            String promotion = product.getPromotion();
 
             printProduct(name, price, quantity, promotion);
         }
-    }
-
-    private static String getPromotion(List<String> product) {
-        String promotion = product.get(INDEX_PROMOTION);
-        if (promotion.equals(NULL)) { return null; }
-
-        return promotion;
     }
 
     private static void printProduct(String name, int price, int quantity, String promotion) {
