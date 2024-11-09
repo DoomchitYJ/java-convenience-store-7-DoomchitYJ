21161d42e050c82b7070d0b5dcdd6298c4e4e521
DoomchitYJ <poj1004@inha.edu>
2024-11-10 00:30:43 +0900

    refactor(PurchasePolicy): 구매 정책 기능 핵심 메서드의 리팩토링 수행

--- src/main/java/store/domain/PurchasePolicy.java
+++ src/main/java/store/domain/PurchasePolicy.java
@@ -1,7 +1,9 @@
 package store.domain;
 
+import static store.constant.Constant.MAX_TRY;
 import static store.constant.Constant.REPLY_NO;
 import static store.constant.Constant.REPLY_YES;
+import static store.view.ErrorPrinter.printError;
 
 import java.util.Date;
 import store.service.CartService;
@@ -20,44 +22,68 @@ public class PurchasePolicy {
 
     private static void applyPromotion(Order order, Promotion promotion) {
         CartService cartService = new CartService();
-        String name = order.getName();
-        int quantity = order.getQuantity();
-        int free = calculateFree(order, promotion);
-
         if (!isPromotionValid(promotion)) {
-            cartService.addToCart(name, quantity, free);
+            applyNoPromotion(cartService, order, promotion);
             return;
         }
-
         if (isBonusPossible(order, promotion)) {
-            String reply = InputView.readMoreFree(order.getName());
-            if (reply.equals(REPLY_YES)) {
-                free++;
-            }
-            cartService.addToCart(name, quantity, free);
+            handleBonusPossible(cartService, order, promotion);
             return;
         }
-
         if (isPromotionStockOut(order)) {
-            int promotionQuantity = InventoryService.getPromotionProductQuantity(order.getName());
-            int left = promotionQuantity - free * (promotion.getBuy()+promotion.getGet());
-            String reply = InputView.readNoDiscount(order.getName(), left);
-            if (reply.equals(REPLY_NO)) {
-                quantity -= left;
+            handleStockOut(cartService, order, promotion);
+            return;
+        }
+        cartService.addToCart(order.getName(), order.getQuantity(), calculateFree(order, promotion));
+    }
+
+    private static void applyNoPromotion(CartService cartService, Order order, Promotion promotion) {
+        cartService.addToCart(order.getName(), order.getQuantity(), calculateFree(order, promotion));
+    }
+
+    private static void handleBonusPossible(CartService cartService, Order order, Promotion promotion) {
+        for (int i = 1; i < MAX_TRY; i++) {
+            try {
+                int free = calculateFree(order, promotion);
+                String reply = InputView.readMoreFree(order.getName());
+                if (reply.equals(REPLY_YES)) {
+                    free++;
+                }
+                cartService.addToCart(order.getName(), order.getQuantity(), free);
+            } catch (IllegalArgumentException e) {
+                printError(e.getMessage());
             }
-            cartService.addToCart(name, quantity, free);
         }
     }
 
-    private static int calculateFree(Order order, Promotion promotion) {
+    private static void handleStockOut(CartService cartService, Order order, Promotion promotion) {
+        for (int i = 1; i < MAX_TRY; i++) {
+            try {
+                int free = calculateFree(order, promotion);
+                int quantity = order.getQuantity();
+                int promotionQuantity = InventoryService.getPromotionProductQuantity(order.getName());
+                int left = promotionQuantity - free * (promotion.getBuy() + promotion.getGet());
+                String reply = InputView.readNoDiscount(order.getName(), left);
+                if (reply.equals(REPLY_NO)) {
+                    quantity -= left;
+                }
+                cartService.addToCart(order.getName(), quantity, free);
+            } catch (IllegalArgumentException e) {
+                printError(e.getMessage());
+            }
+
+        }
+    }
+
+    private static int calculateFree (Order order, Promotion promotion){
         int promotionQuantity = InventoryService.getPromotionProductQuantity(order.getName());
         if (promotionQuantity >= order.getQuantity()) {
-            return (int) (order.getQuantity() / (promotion.getBuy() + promotion.getGet()));
+            return (order.getQuantity() / (promotion.getBuy() + promotion.getGet()));
         }
-        return (int) (promotionQuantity / (promotion.getBuy() + promotion.getGet()));
+        return (promotionQuantity / (promotion.getBuy() + promotion.getGet()));
     }
 
-    private static boolean isPromotionValid(Promotion promotion) {
+    private static boolean isPromotionValid (Promotion promotion){
         if (promotion == null) {
             return false;
         }
@@ -65,13 +91,13 @@ public class PurchasePolicy {
         return today.after(promotion.getStartDate()) && today.before(promotion.getEndDate());
     }
 
-    private static boolean isBonusPossible(Order order, Promotion promotion) {
+    private static boolean isBonusPossible (Order order, Promotion promotion){
         return order.getQuantity() % (promotion.getBuy() + promotion.getGet()) == promotion.getBuy()
                 && InventoryService.getPromotionProductQuantity(order.getName())
                 > order.getQuantity() + promotion.getGet();
     }
 
-    private static boolean isPromotionStockOut(Order order) {
+    private static boolean isPromotionStockOut (Order order){
         return order.getQuantity() > InventoryService.getPromotionProductQuantity(order.getName());
     }
 }
