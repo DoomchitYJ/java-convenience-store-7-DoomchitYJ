67841af3330d441148d1c4393f6165e9e909e5f9
DoomchitYJ <poj1004@inha.edu>
2024-11-09 23:40:20 +0900

    test(InventoryServiceTest): InventoryService의 구조 변경에 따른 테스트 코드 수정

--- src/test/java/store/service/InventoryServiceTest.java
+++ src/test/java/store/service/InventoryServiceTest.java
@@ -1,37 +1,21 @@
 package store.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
-import store.domain.Product;
 
 public class InventoryServiceTest {
 
     @Test
-    void 최초_재고_셋업을_수행한다() {
+    void 상품이_존재할_경우_참을_반환한다() {
 
-        // given & when
-        List<Product> inventory = InventoryService.setUpInventory();
-        String name1 = inventory.getFirst().getName();
-        int price1 = inventory.getFirst().getPrice();
-        int quantity1 = inventory.getFirst().getQuantity();
-        String promotion1 = inventory.getFirst().getPromotion();
+        assertTrue(InventoryService.isProductExistent("콜라"));
+    }
 
-        String name2 = inventory.get(3).getName();
-        int price2 = inventory.get(3).getPrice();
-        int quantity2 = inventory.get(3).getQuantity();
-        String promotion2 = inventory.get(3).getPromotion();
+    @Test
+    void 존재하지_않을_상품일_경우_거짓을_반환한다() {
 
-        // then
-        assertEquals("콜라", name1);
-        assertEquals(1000, price1);
-        assertEquals(10, quantity1);
-        assertEquals("탄산2+1", promotion1);
-
-        assertEquals("사이다", name2);
-        assertEquals(1000, price2);
-        assertEquals(7, quantity2);
-        assertEquals(null, promotion2);
+        assertFalse(InventoryService.isProductExistent("반창고"));
     }
 }
