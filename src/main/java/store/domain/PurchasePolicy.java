52518a3fa554428328cb32b0201f186f2ccff316
DoomchitYJ <poj1004@inha.edu>
2024-11-09 22:53:39 +0900

    refactor(PromotionsRepository): PromotionsRepository 책임 변경
    - 기존 promotions.md 파일을 읽어오는 기능에서 읽고 파싱해서 List<Promotion>을 반환하는 기능을 수행하도록 변경

--- src/main/java/store/domain/PurchasePolicy.java
+++ src/main/java/store/domain/PurchasePolicy.java
@@ -1,76 +1,22 @@
 package store.domain;
 
-import static store.config.Congfig.PROMOTIONS_FILE_PATH;
-import static store.constant.Constant.DATE_FORMAT;
-import static store.constant.Constant.INDEX_BUY;
-import static store.constant.Constant.INDEX_END_DATE;
-import static store.constant.Constant.INDEX_GET;
-import static store.constant.Constant.INDEX_NAME;
-import static store.constant.Constant.INDEX_START_DATE;
 import static store.constant.Constant.REPLY_NO;
 import static store.constant.Constant.REPLY_YES;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import store.repository.PromotionsRepository;
 import store.service.CartService;
 import store.service.InventoryService;
 import store.view.InputView;
 
 public class PurchasePolicy {
 
-    private static final List<Promotion> promotions = loadPromotions();
+    private static final Promotions promotions = new Promotions();
 
     public static void buy(Order order) {
-        Promotion promotion = findPromotionByName(InventoryService.getPromotionNameByProductName(order.getName()));
+        Promotion promotion = promotions.findPromotionByName(
+                InventoryService.getPromotionNameByProductName(order.getName()));
         applyPromotion(order, promotion);
     }
-
-    private static List<Promotion> loadPromotions() {
-        PromotionsRepository promotionsRepository = new PromotionsRepository(PROMOTIONS_FILE_PATH);
-        List<Promotion> promotions = new ArrayList<>();
-        try {
-            List<List<String>> promotionsFileContents = promotionsRepository.readPromotionsFile();
-            for (List<String> content : promotionsFileContents) {
-                Promotion promotion = convertIntoPromotion(content);
-                promotions.add(promotion);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return promotions;
-    }
-
-    private static Promotion convertIntoPromotion(List<String> content) {
-        String name = content.get(INDEX_NAME);
-        int buy = Integer.parseInt(content.get(INDEX_BUY));
-        int get = Integer.parseInt(content.get(INDEX_GET));
-        Date startDate = getDate(content.get(INDEX_START_DATE));
-        Date endDate = getDate(content.get(INDEX_END_DATE));
-
-        return new Promotion(name, buy, get, startDate, endDate);
-    }
-
-    private static Date getDate(String dateInString) {
-        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
-        try {
-            return(formatter.parse(dateInString));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static Promotion findPromotionByName(String promotionName) {
-        return promotions.stream()
-                .filter(p -> p.getName().equals(promotionName))
-                .findFirst().orElse(null);
-    }
-
 
     private static void applyPromotion(Order order, Promotion promotion) {
         CartService cartService = new CartService();
--- src/main/java/store/repository/PromotionsRepository.java
+++ src/main/java/store/repository/PromotionsRepository.java
@@ -1,13 +1,26 @@
 package store.repository;
 
+import static store.constant.Constant.CONTENTS_START_INDEX;
+import static store.constant.Constant.DATE_FORMAT;
 import static store.constant.Constant.DELIMITER;
+import static store.constant.Constant.INDEX_BUY;
+import static store.constant.Constant.INDEX_END_DATE;
+import static store.constant.Constant.INDEX_GET;
+import static store.constant.Constant.INDEX_NAME;
+import static store.constant.Constant.INDEX_START_DATE;
+import static store.exception.ExceptionMessage.FILE_PATH_ERROR;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import store.domain.Promotion;
+import store.exception.StoreException;
 
 public class PromotionsRepository {
 
@@ -19,28 +32,36 @@ public class PromotionsRepository {
         this.filePath = Paths.get(filePath);
     }
 
-    public List<List<String>> readPromotionsFile() throws IOException {
-        List<String> lines = Files.readAllLines(filePath);
-        String[] header = lines.getFirst().split(DELIMITER);
-
-        List<List<String>> promotions = addPromotions(lines, header);
-
-        return promotions;
+    public List<Promotion> loadPromotions() {
+        try {
+            List<String> lines = Files.readAllLines(filePath);
+            List<Promotion> promotions = new ArrayList<>();
+            for (int i = CONTENTS_START_INDEX; i < lines.size(); i++) {
+                String[] promotion = lines.get(i).split(DELIMITER);
+                promotions.add(convertIntoPromotion(promotion));
+            }
+            return promotions;
+        } catch (IOException e) {
+            throw new StoreException(FILE_PATH_ERROR);
+        }
     }
 
-    private List<List<String>> addPromotions(List<String> lines, String[] header) {
-        List<List<String>> promotions = new ArrayList<>();
+    private static Promotion convertIntoPromotion(String[] content) {
+        String name = content[INDEX_NAME];
+        int buy = Integer.parseInt(content[INDEX_BUY]);
+        int get = Integer.parseInt(content[INDEX_GET]);
+        Date startDate = getDate(content[INDEX_START_DATE]);
+        Date endDate = getDate(content[INDEX_END_DATE]);
 
-        for (int i = PROMOTIONS_START_INDEX; i < lines.size(); i++) {
-            String[] line = lines.get(i).split(DELIMITER);
-            List<String> promotionInfo = new ArrayList<>();
+        return new Promotion(name, buy, get, startDate, endDate);
+    }
 
-            for (int j = 0; j < header.length; j++) {
-                promotionInfo.add(header[j].trim());
-            }
-            promotions.add(promotionInfo);
+    private static Date getDate(String dateInString) {
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        try {
+            return(formatter.parse(dateInString));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
-
-        return promotions;
     }
 }
