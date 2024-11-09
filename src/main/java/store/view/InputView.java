329ba7bf2f9d949b8b0465f39d0ffd8ac468a2da
DoomchitYJ <poj1004@inha.edu>
2024-11-09 22:02:51 +0900

    fix(InputView): [상품명-수량] 형식에 맞는지 확인하는 기능 수정
    - matcher.find() 추가 작성

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -18,7 +18,7 @@ import store.exception.StoreException;
 public class InputView {
 
     private static final String BUYING_VIEW = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
-    private static final String ORDER_REGEX = "\\[\\p{L}+\\-\\d+\\]";
+    private static final String ORDER_REGEX = "\\[([가-힣]+)-(\\d+)]";
 
     private static final String MORE_FREE_VIEW = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n";
 
@@ -52,16 +52,18 @@ public class InputView {
 
     private static String parseName(Pattern pattern, String input) {
         Matcher matcher = pattern.matcher(input);
-        String name = matcher.group(1);
-
-        return name;
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        throw new StoreException(INVALID_FORMAT);
     }
 
     private static int parseQuantity(Pattern pattern, String input) {
         Matcher matcher = pattern.matcher(input);
-        int quantity = Integer.parseInt(matcher.group(1));
-
-        return quantity;
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group(2));
+        }
+        throw new StoreException(INVALID_FORMAT);
     }
 
     public static String readMoreFree(String name) {
