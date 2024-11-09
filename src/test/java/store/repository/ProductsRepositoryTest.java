82bf208b6a14de96c152a79c681e60b7055d0685
DoomchitYJ <poj1004@inha.edu>
2024-11-09 23:39:42 +0900

    test(ProductsRepositoryTest): ProductsRepository의 구조 변경에 따른 테스트 코드 수정

--- src/test/java/store/repository/ProductsRepositoryTest.java
+++ src/test/java/store/repository/ProductsRepositoryTest.java
@@ -3,42 +3,105 @@ package store.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static store.config.Congfig.PRODUCTS_FILE_PATH;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import store.domain.Product;
 
 public class ProductsRepositoryTest {
 
     @Test
-    void products_md_파일을_읽는다() {
+    void products_md_를_읽고_재고를_세팅한다() {
         // given
         ProductsRepository productsRepository = new ProductsRepository(PRODUCTS_FILE_PATH);
 
         // when
-        List<List<String>> products;
-        try {
-            products = productsRepository.readProductsFile();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        List<Product> inventory = productsRepository.loadInventory();
+        List<String> name = new ArrayList<>();
+        List<Integer> price = new ArrayList<>();
+        List<Integer> quantity = new ArrayList<>();
+        List<String> promotion = new ArrayList<>();
+        for (Product product : inventory) {
+            name.add(product.getName());
+            price.add(product.getPrice());
+            quantity.add(product.getQuantity());
+            promotion.add(product.getPromotion());
         }
 
         // then
-        List<List<String>> expected = List.of(
-                List.of("콜라","1000","10","탄산2+1"),
-                List.of("콜라","1000","10","null"),
-                List.of("사이다","1000","8","탄산2+1"),
-                List.of("사이다","1000","7","null"),
-                List.of("오렌지주스","1800","9","MD추천상품"),
-                List.of("탄산수","1200","5","탄산2+1"),
-                List.of("물","500","10","null"),
-                List.of("비타민워터","1500","6","null"),
-                List.of("감자칩","1500","5","반짝할인"),
-                List.of("감자칩","1500","5","null"),
-                List.of("초코바","1200","5","MD추천상품"),
-                List.of("초코바","1200","5","null"),
-                List.of("에너지바","2000","5","null"),
-                List.of("정식도시락","6400","8","null"),
-                List.of("컵라면","1700","1","MD추천상품"),
-                List.of("컵라면","1700","10","null"));
-        assertEquals(products, expected);
+        List<String> expectedName = List.of("콜라",
+                "콜라",
+                "사이다",
+                "사이다",
+                "오렌지주스",
+                "탄산수",
+                "물",
+                "비타민워터",
+                "감자칩",
+                "감자칩",
+                "초코바",
+                "초코바",
+                "에너지바",
+                "정식도시락",
+                "컵라면",
+                "컵라면");
+        List<Integer> expectedPrice = List.of(
+                1000,
+                1000,
+                1000,
+                1000,
+                1800,
+                1200,
+                500,
+                1500,
+                1500,
+                1500,
+                1200,
+                1200,
+                2000,
+                6400,
+                1700,
+                1700
+        );
+        List<Integer> expectedQuantity = List.of(
+                10,
+                10,
+                8,
+                7,
+                9,
+                5,
+                10,
+                6,
+                5,
+                5,
+                5,
+                5,
+                5,
+                8,
+                1,
+                10
+        );
+        List<String> expectedPromotion = List.of(
+                "탄산2+1",
+                "",
+                "탄산2+1",
+                "",
+                "MD추천상품",
+                "탄산2+1",
+                "",
+                "",
+                "반짝할인",
+                "",
+                "MD추천상품",
+                "",
+                "",
+                "",
+                "MD추천상품",
+                ""
+        );
+        assertEquals(expectedName, name);
+        assertEquals(expectedPrice, price);
+        assertEquals(expectedQuantity, quantity);
+        assertEquals(expectedPromotion, promotion);
     }
 }
