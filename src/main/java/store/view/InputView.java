8a8901be9eaec18d3525aa2cecd856bf1fa1d37a
DoomchitYJ <poj1004@inha.edu>
2024-11-09 19:01:07 +0900

    feat(InputView): 프로모션 적용 가능 상품을 고객이 해당 수량만큼 가져오지 않았을 경우, 안내 메시지를 출력하고 응답을 입력 받는 기능 구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -10,13 +10,14 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import store.domain.Order;
-import store.exception.ExceptionMessage;
 import store.exception.StoreException;
 
 public class InputView {
 
     private static final String BUYING_VIEW = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String ORDER_REGEX = "\\[\\p{L}+\\-\\d+\\]";
+
+    private static final String MORE_FREE_VIEW = "현재 {상품명}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
 
     public static List<Order> readOrder() {
         System.out.println(BUYING_VIEW);
@@ -53,6 +54,12 @@ public class InputView {
         int quantity = Integer.parseInt(matcher.group(1));
 
         return quantity;
+    }
+
+    public static String readMoreFree() {
+        System.out.println(MORE_FREE_VIEW);
+
+        return Console.readLine().trim();
     }
 }
 
